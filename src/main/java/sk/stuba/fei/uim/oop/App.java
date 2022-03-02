3bcd9ef031bf2f9f34b20575d6be8269526ed3b7
DominikSmidak <xsmidak@stuba.sk>
2022-03-02 11:16:09 +0100

    polia

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,7 +2,7 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int celeCislo = 5;
+        /*int celeCislo = 5;
         long celeVacsieCislo = 5L;
         float desatinneCislo = 5.5f;
         double dlhsieDesatinneCislo = 5.5;
@@ -17,6 +17,36 @@ public class App {
         System.out.println(zvysok);
 
         float podielFloat = 2/3.0f;
-        System.out.println(podielFloat);
+        System.out.println(podielFloat);*/ //premenne
+        /*int cislo = Integer.parseInt(args[0]);
+        if(cislo == 0) {
+            System.out.println("je to nula");
+        } else {
+            System.out.println("nie je to nula");
+        }*/ //if
+        /*int cislo = 1;
+        while( cislo<4 ) {
+            switch (cislo) {
+                case 1:
+                    System.out.println("Cislo je 1");
+                    break;
+                case 2:
+                    System.out.println("Cislo je 2");
+                    break;
+                default:
+                    System.out.println("Cislo je ine ako 1 alebo 2");
+            }
+            cislo++;
+        }*/ //switch + while
+        int[] pole = new int[10];
+        for(int i = 0; i< pole.length; i++){
+            pole[i]=i;
+        }
+        for(int i = 0; i< pole.length; i++){
+            App.vypis(i);
+        }
+    }
+    public static void vypis(int a){
+        System.out.println(a);
     }
 }
